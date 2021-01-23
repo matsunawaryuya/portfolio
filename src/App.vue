2814fa27c392b13cfefb104a7d5baa5e4b279fe8
matsunawaryuya <ruymtnw@gmail.com>
2021-01-23 16:18:28 +0900

    install yarn pug

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="pug">
+img(alt='Vue logo' src='./assets/logo.png')
+hello-world(msg='Welcome to Your Vue.js App')
 </template>
 
 <script>
+import { defineComponent } from '@vue/composition-api';
 import HelloWorld from './components/HelloWorld.vue'
 
-export default {
+export default defineComponent({
   name: 'App',
   components: {
     HelloWorld
   }
-}
+});
 </script>
 
 <style>
